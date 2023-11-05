170794f4189ecebf724281529108e712badd294c
mosesvk <kaumatule93@gmail.com>
2023-11-04 19:56:38 -0600

    fix year selection, local storage update duplicate data

---
+++ 04_k12/.history/components/charts_20231104193204.js
@@ -0,0 +1,153 @@
+const getMainChartOptions = (dataPeer, dataClient) => {
+
+  const clientArray = []
+  let chartColor = '#3a464f';
+  let peerAvg = [];
+  let peerMid = [];
+  let peerMin = [];
+  let peerMax = [];
+  let mainChartColors
+
+  if (document.documentElement.classList.contains('dark')) {
+    mainChartColors = {
+      borderColor: '#374151',
+      labelColor: '#ebedf0',
+      opacityFrom: 0,
+      opacityTo: 0.15
+    }
+
+    chartColor = '#e3f0fa';
+  } else {
+    mainChartColors = {
+      borderColor: '#F3F4F6',
+      labelColor: '#6B7280',
+      opacityFrom: 0.45,
+      opacityTo: 0
+    };
+  }
+
+
+  let selectedYearsArray = getSelectedYearsFromLocalStorage();
+
+
+  selectedYearsArray.forEach((year) => {
+    // Check if the dataPeer array for the selected year exists
+    if (dataPeer[year]) {
+      const array = dataPeer[year];
+  
+      const avg = getAverageOfArray(array);
+      const mid = getMidpointOfArray(array);
+      const min = Math.min(...array);
+      const max = Math.max(...array);
+  
+      peerAvg.push(avg);
+      peerMid.push(mid);
+      peerMin.push(min);
+      peerMax.push(max);
+      clientArray.push(dataClient[year][0]);
+    } else {
+      // Handle the case when dataPeer for the year is not defined
+      // You can add error handling or default values as needed
+      console.error(`Data for year ${year} is undefined in dataPeer`);
+    }
+  });
+  
+  return {
+    colors: [
+      window.chartColors.green,
+      window.chartColors.blue,
+      window.chartColors.orange
+    ],
+    series: [
+      {
+        name: 'Cashflow',
+        type: 'column',
+        data: clientArray,
+        style: {
+          colors: [mainChartColors.labelColor]
+        }
+      },
+      {
+        name: 'Avg',
+        type: 'line',
+        data: peerAvg,
+        yaxis: 0
+      },
+      {
+        name: 'Mid',
+        type: 'line',
+        data: peerMid,
+      }
+    ],
+    chart: {
+      height: 350,
+      type: 'line',
+      stacked: false
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      width: [2, 6, 4]
+    },
+    title: {
+      text: '',
+      align: 'left',
+      offsetX: 110
+    },
+    xaxis: {
+      categories: selectedYearsArray,
+      labels: {
+        style: {
+          colors: mainChartColors.labelColor
+        }
+      }, 
+    },
+    yaxis: [
+      {
+        axisTicks: {
+          show: true
+        },
+        axisBorder: {
+          show: true,
+          color: chartColor
+        },
+        labels: {
+          style: {
+            colors: chartColor,
+            fontSize: '1.25rem' // Set the desired font size here
+          }
+        },
+        tooltip: {
+          enabled: true
+        },
+
+      }
+    ],
+    tooltip: {
+      fixed: {
+        enabled: true,
+        position: 'topLeft',
+        offsetY: 30,
+        offsetX: 60
+      }
+    },
+    legend: {
+      horizontalAlign: 'left',
+      offsetX: 40
+    },
+    grid: {
+      row: {
+        colors: ['transparent'],
+        opacity: 0.5,
+        thickness: 4
+      }
+    },
+    plotOptions: {
+      bar: {
+        barHeight: '70%', // Set the bar height to 90% of available space
+      }
+    }
+  };
+  
+};
---
+++ 04_k12/.history/components/comp_Report_20231104140525.js
@@ -0,0 +1,233 @@
+const displayReportComponent = () => {
+  const enrollmentTable = `
+  <div class='p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800'>
+  <div class='flex flex-col mt-2'>
+    <div class='overflow-x-auto rounded-lg'>
+      <div class='inline-block min-w-full align-middle'>
+        <div class='relative overflow-x-auto shadow-md sm:rounded-lg'>
+          <table class='w-full text-lg text-left text-gray-500 dark:text-gray-400'>
+            <thead class='text-xs text-gray-700 uppercase bg-green-200 dark:bg-gray-700 dark:text-green-200 '>
+              <tr id='row_enrollment_tableHeader'>
+                <th scope='col' class='px-6 py-3 text-lg'>
+                  Enrollment Comparison between Years
+                </th>
+
+                <th scope='col' class='px-6 py-3'>
+                  Avg
+                </th>
+                <th scope='col' class='px-6 py-3'>
+                  Mid
+                </th>
+                <th scope='col' class='px-6 py-3'>
+                  Min
+                </th>
+                <th scope='col' class='px-6 py-3'>
+                  Max
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                id='row_studentsAverageEnrollment'
+                class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+              >
+                <th
+                  scope='row'
+                  class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                >
+                  Students - Average Enrollment
+                </th>
+              </tr>
+              <tr
+                id='row_studentsAverageEnrollment-percentChange'
+                class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+              >
+                <th
+                  scope='row'
+                  class='pr-6 pl-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                >
+                  a. % Change
+                </th>
+              </tr>
+              <tr
+                id='row_studentsAverageEnrollment-Average'
+                class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+              >
+                <th
+                  scope='row'
+                  class='pr-6 pl-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                >
+                  b. Average Enrollment
+                </th>
+              </tr>
+              <tr
+                id='row_studentsAverageEnrollment-Peak'
+                class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+              >
+                <th
+                  scope='row'
+                  class='pr-6 pl-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                >
+                  b. Peak Enrollment
+                </th>
+              </tr>
+              <tr
+                id='row_studentsAverageEnrollment-Average'
+                class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+              >
+                <th
+                  scope='row'
+                  class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                >
+                  Student/Faculty Ratio
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  `;
+
+  const cashTable = `
+  <div class='p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800'>
+      <div class='flex flex-col mt-2'>
+        <div class='overflow-x-auto rounded-lg'>
+          <div class='inline-block min-w-full align-middle'>
+            <div class='relative overflow-x-auto shadow-md sm:rounded-lg'>
+              <table class='w-full text-lg text-left text-gray-500 dark:text-gray-400'>
+                <thead class='text-xs text-gray-700 uppercase bg-green-200 dark:bg-gray-700 dark:text-green-200'>
+                  <tr id='row_cash_tableHeader'>
+                    <th scope='col' class='px-6 py-3 text-lg'>
+                      Cash Flow and Reserve Ratios
+                    </th>
+
+                    <th scope='col' class='px-6 py-3'>
+                      Avg
+                    </th>
+                    <th scope='col' class='px-6 py-3'>
+                      Mid
+                    </th>
+                    <th scope='col' class='px-6 py-3'>
+                      Min
+                    </th>
+                    <th scope='col' class='px-6 py-3'>
+                      Max
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    id='row_expendableReserves_inDays'
+                    class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                  >
+                    <th
+                      scope='row'
+                      class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                    >
+                      1. Expendable Reserves - In Days
+                    </th>
+                  </tr>
+                  <tr
+                    id='row_expendableReserves_percentTotalCash'
+                    class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                  >
+                    <th
+                      scope='row'
+                      class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                    >
+                      2. Expendable Reserves - % of Total Cash Exprenses
+                    </th>
+                  </tr>
+                  <tr
+                    id='row_cashAvailableDeferred'
+                    class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                  >
+                    <th
+                      scope='row'
+                      class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                    >
+                      3. Cash Available to Deferred Revenues
+                    </th>
+                  </tr>
+                  <tr
+                    id='row_liquidityRatio'
+                    class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                  >
+                    <th
+                      scope='row'
+                      class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                    >
+                      4. Liquidity Ratio
+                    </th>
+                  </tr>
+                  <tr
+                    id='row_netCashUsedOperating'
+                    class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                  >
+                    <th
+                      scope='row'
+                      class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                    >
+                      5. Net Cash Provided (used) by Operating Activities
+                    </th>
+                  </tr>
+                  <tr
+                    id='row_netCashUsedOperating_asPerStatementCash'
+                    class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                  >
+                    <th
+                      scope='row'
+                      class='pr-6 pl-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                    >
+                      a. As per statement of Cash Flows
+                    </th>
+                  </tr>
+                  <tr
+                    id='row_netCashUsedOperating_depreciationExpensesThreeToSeven'
+                    class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                  >
+                    <th
+                      scope='row'
+                      class='pr-6 pl-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                    >
+                      b. Depreciation Expenses on 3 - 7 Year Assets
+                    </th>
+                  </tr>
+                  <tr
+                    id='row_netCashUsedOperating_overUnderBench'
+                    class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                  >
+                    <th
+                      scope='row'
+                      class='pr-6 pl-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                    >
+                      c. Over (Under) Benchmark
+                    </th>
+                  </tr>
+                  
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  document.querySelector('main').innerHTML = `
+    ${enrollmentTable}
+    ${cashTable}
+  `;
+
+  const savedData = localStorage.getItem('enrollmentData');
+  const selectedYears = localStorage.getItem('selectedYears')
+
+  if (savedData && localStorage.getItem('selectedYears')) {
+    addTableColumnsToReport('row_enrollment_tableHeader', selectedYears);
+  }
+
+  closeSidebarAfterSelectingOption('report');
+};
