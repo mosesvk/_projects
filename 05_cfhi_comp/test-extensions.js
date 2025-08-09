3b8247b8192942d15d7b90db0134b8b212315ccc
mosesvk <kaumatule93@gmail.com>
2025-08-09 07:04:31 -0600

    update

---
+++ 05_cfhi_comp/test-extensions.js
@@ -0,0 +1,9 @@
+d// Test file to verify extensions are working
+const testFunction = () => {
+  const testVar = "hello world"
+  console.log(testVar)
+  return testVar
+}
+
+// This should trigger ESLint warnings about missing semicolons
+// and Prettier should format this code when you use Format Document
