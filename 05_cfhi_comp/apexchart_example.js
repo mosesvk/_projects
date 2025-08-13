5fd872604d8bf595af462313c6aec70664b8f174
mosesvk <kaumatule93@gmail.com>
2025-08-13 11:15:37 -0600

    updagte

--- 05_cfhi_comp/apexchart_example.js
+++ 05_cfhi_comp/apexchart_example.js
@@ -1,1 +1,29 @@
 
+// {
+//   "chart": {
+//     "caption": "Quarterly Sales",
+//     "xAxisName": "Quarter",
+//     "yAxisName": "Sales",
+//     "showValues": "1",
+//     "labelBgColor": "#ADD8E6" // Light blue background for all data labels
+//   },
+//   "data": [
+//     {
+//       "label": "Q1",
+//       "value": "420000"
+//     },
+//     {
+//       "label": "Q2",
+//       "value": "295000",
+//       "labelBgColor": "#FFD700" // Golden background for Q2's data label
+//     },
+//     {
+//       "label": "Q3",
+//       "value": "523000"
+//     },
+//     {
+//       "label": "Q4",
+//       "value": "465000"
+//     }
+//   ]
+// }
