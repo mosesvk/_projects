e78d3201caa5bd2fa9b16675733157f68353242b
mosesvk <kaumatule93@gmail.com>
2025-08-20 20:40:21 -0600

    update

--- 05_cfhi_comp/src/components/Report.js
+++ 05_cfhi_comp/src/components/Report.js
@@ -414,7 +414,7 @@ const addSingleNewColumnToReportTable = (tableHeader, yearsArray) => {
     // Create a new <th> element for each selected year
     const newTh = document.createElement("th");
     newTh.setAttribute("scope", "col");
-    newTh.setAttribute("class", "px-6 py-3");
+    newTh.setAttribute("class", "px-6 py-3 text-lg");
     newTh.innerText = year;
 
     // Insert the new <th> element before the "avg" <th>
