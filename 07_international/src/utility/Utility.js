ed8d9bf3576f6c85dd9ddc6659322ce59fb128e9
mosesvk <kaumatule93@gmail.com>
2025-09-29 19:36:05 -0600

    update

--- 07_international/src/utility/Utility.js
+++ 07_international/src/utility/Utility.js
@@ -120,7 +120,7 @@ const createToastWarning = (textString) => {
     "flex",
     "items-center",
     "w-full",
-    "max-w-md",
+    "max-w-lg",
     "p-4",
     "text-gray-700",
     "bg-gray-300",
@@ -204,7 +204,7 @@ const createToastSuccess = (textString) => {
       </svg>
       <span class="sr-only">success</span>
     </div>
-    <div class="ms-3 text-sm font-normal">${textString}</div>
+    <div class="ms-3 text-lg font-normal">${textString}</div>
     <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
         <span class="sr-only">Close</span>
         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
