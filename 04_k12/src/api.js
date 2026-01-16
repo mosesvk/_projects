fe05470c2b60136693314fbb7504d07b94ecdb77
mosesvk <kaumatule93@gmail.com>
2026-01-16 12:01:32 -0700

    update

--- 04_k12/src/api.js
+++ 04_k12/src/api.js
@@ -67,7 +67,7 @@ const insertDataIntoObject = (
   dynamicValueClientPeer,
   name
 ) => {
-  // console.log({ type, year, object, dataKey, record, child, dynamicValueClientPeer, name });
+  console.log({ type, year, object, dataKey, record, child, dynamicValueClientPeer, name });
   const innerData =
     record.querySelector(child).innerHTML.split("").length > 0
       ? record.querySelector(child).innerHTML.trim()
